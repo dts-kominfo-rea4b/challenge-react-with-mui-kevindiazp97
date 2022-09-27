f050bd65bd58679c1cdf243468a6173302392a8d
kevindiazp97 <kevindiazp97@gmail.com>
2022-09-27 16:44:34 +0700

    Add challenge form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,35 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
+import {ListItem, ListItemText, ListItemAvatar, Avatar, Typography} from '@mui/material';
+import Data from "../data/contacts.json"
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (<>
+    <list sx={{ width: "100%", maxWidth: 360 }}>
+        <ListItem alignItems='flex-start'>
+            <ListItemAvatar>
+                <Avatar alt = "Remy Sharp" src={data.photo}/>
+            </ListItemAvatar>
+            <ListItemText primary = {data.name}
+            secondary = {
+                <React.Fragment>
+                    <Typography sx = {{display:"inline"}}
+                    component = "span"
+                    variant = "body2"
+                    color = "text.primary">
+                    </Typography>
+                    <p>{data.phone}</p>
+                    <p>{data.email}</p>
+                </React.Fragment>
+            }
+            />
+        </ListItem>
+    </list>
+    </>);
 };
 
 export default Contact;
